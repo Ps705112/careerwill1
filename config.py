4d76a6dab2314e92b14baf6c8d429ddb8423f1e6
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2024-03-27 23:33:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6415777909:AAE72fd1b1GVkICgNRll8OvHhplORD_57qg"
+    BOT_TOKEN = "5688426079:AAHjy-kbSWpyRrTQJ3IDDsKEGWn8hBGLvuU"
     API_ID = "15816419"
     API_HASH = "626ed6dab78881858778d9663682962e"
-    AUTH_USERS = '2088265361'
+    AUTH_USERS = '966029924'
     
